e40df42bf52618ce47ff3f407ca16aaaae080f66
arhamsc <shaik.arham@gmail.com>
2023-10-09 23:40:40 +0530

    Dynamic to string added

---
+++ json_serializable_transformers/lib/src/dynamic_to_string.dart
@@ -0,0 +1,30 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+///Implement this class transformer to convert a int type to String.
+///
+///
+///Example: 20 to "20"
+///
+///It can be used with `freezed` or `json_serializable` packages.
+///
+///To implement this, just annotate the json key with this class.
+///
+///```dart
+///@freezed
+///class MyFreezedClass {
+///...
+///@DynamicToStringTransformer()
+///Value property,
+///...
+///}
+///```
+///
+class DynamicToStringTransformer implements JsonConverter<String, dynamic> {
+  const DynamicToStringTransformer();
+
+  @override
+  String fromJson(dynamic json) => json is String ? json : json.toString();
+
+  @override
+  dynamic toJson(String object) => int.parse(object);
+}
--- json_serializable_transformers/lib/src/index.dart
+++ json_serializable_transformers/lib/src/index.dart
@@ -5,5 +5,6 @@ export "./string_to_int_transformer.dart";
 export "./to_string_transformer.dart";
 export "./int_to_string_transformer.dart";
 export "./double_to_int_transformer.dart";
+export "./dynamic_to_string.dart";
 
 export "./utils/index.dart";

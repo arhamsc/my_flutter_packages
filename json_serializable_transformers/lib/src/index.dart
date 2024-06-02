f0dce9f4a5ab3f0dfffd9335f57ff7d0c1309722
arhamsc <shaik.arham@gmail.com>
2024-06-02 11:51:38 +0530

    added file transformer import

--- json_serializable_transformers/lib/src/index.dart
+++ json_serializable_transformers/lib/src/index.dart
@@ -7,5 +7,6 @@ export "numeric_transformers/int_to_string_transformer.dart";
 export "numeric_transformers/double_to_int_transformer.dart";
 export "string_transformers/dynamic_to_string.dart";
 export "network_transformers/internet_address_transformer.dart";
+export "image_transformers/selected_file_transformer.dart";
 
 export "./utils/index.dart";
